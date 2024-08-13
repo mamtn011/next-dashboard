1bf8dbffd84755209f604e4054682406f92d9042
Mobin <mamtn011@gmail.com>
2024-08-13 23:23:16 +0600

    validate update invoice form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,14 +13,6 @@ export type State = {
   message?: string | null;
 };
 // validation schema
-// const FormSchema = z.object({
-//   id: z.string(),
-//   customerId: z.string(),
-//   amount: z.coerce.number(),
-//   status: z.enum(["pending", "paid"]),
-//   date: z.string(),
-// });
-
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -76,12 +68,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 
 // update
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Update Invoice.",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
 
   const amountInCents = amount * 100;
 
